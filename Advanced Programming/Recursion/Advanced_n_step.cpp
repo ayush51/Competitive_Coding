88ad345142ed638e9e0ffa2be53613e3aaf8c0b1
Ayush Aggarwal <Aama098@gmail.com>
2018-10-05 23:29:08 +0530

    fully  solved

---
+++ Advanced Programming/Recursion/Advanced_n_step.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+step(int n, int i)
+{
+if(n==0)
+return 1;
+int ans = 0;
+if (n>=1){
+for(int j = 1; j<=i && j<=n ; j++)
+ans += step(n-j, i);
+}
+return ans;
+}
+int main()
+{
+int n,i;
+cin>>n>>i;
+cout<<step(n,i);
+return 0;
+}
