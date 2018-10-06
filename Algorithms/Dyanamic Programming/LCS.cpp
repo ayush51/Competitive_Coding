ecbd64a9fc4a53acd1ce4e24f0897ad7b8e05935
Ayush Aggarwal <Aama098@gmail.com>
2018-10-06 20:29:43 +0530

    starting

---
+++ Algorithms/Dyanamic Programming/LCS.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+// it will find the length of common sequence
+ int lcs(char *X,char *Y,int m, int n){
+int L[m+1][n+1];//it will store our common length trick
+int i,j;
+
+ //following steps build the L[m+1][n+1] in bottom up fashion
+// and it keep track of the longest common sequence
