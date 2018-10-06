dbdd59605d1161dbe6faac933eac35f3812c2914
Ayush Aggarwal <Aama098@gmail.com>
2018-10-06 20:30:18 +0530

    updating

--- Algorithms/Dyanamic Programming/LCS.cpp
+++ Algorithms/Dyanamic Programming/LCS.cpp
@@ -7,3 +7,11 @@ int i,j;
 
  //following steps build the L[m+1][n+1] in bottom up fashion
 // and it keep track of the longest common sequence
+
+  for(i=0;i<=m;i++){
+   for (j=0;j<=n;j++)
+   {
+     if (i==0 || j==0){
+       L[i][j]=0;// making the first row and first column all elements equal to zero
+
+     }
